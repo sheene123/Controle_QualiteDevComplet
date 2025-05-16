b33407fb41b86276efbfb8c7c1d2c5845e02918d
SIMILIEN Carld <12300697@q10624.iutv.univ-paris13.fr>
2025-05-16 09:16:47 +0200

    TP1_fini

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
@@ -9,9 +10,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class VoitureTest {
 
+    Voiture v1;
+
+    @BeforeEach
+    void CreationVoiture(){
+        this.v1 = new Voiture("Audi",15000);
+    }
     @Test
     void creerVoiture(){
         assertEquals(1,1);
     }
 
+    @Test
+    void  TestVoiture(){
+
+        assertEquals("Audi",v1.getMarque());
+        assertEquals(15000,v1.getPrix());
+        assertEquals(0,v1.getId());
+
+        String expectedToString = "Car{marque='Audi', prix=15000, id=0}";
+        assertEquals(expectedToString, v1.toString());
+
+        v1.setId(1);
+        v1.setMarque("Renault");
+        v1.setPrix(10000);
+
+        assertEquals(1,v1.getId());
+        assertEquals("Renault",v1.getMarque());
+        assertEquals(10000,v1.getPrix());
+
+    }
 }
