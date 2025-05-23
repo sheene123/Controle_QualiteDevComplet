479b3619688416fb94d37c6ee127b4c8f4bec66a
SIMILIEN Carld <12300697@q10421.iutv.univ-paris13.fr>
2025-05-23 11:49:38 +0200

    exercice 1 fini avec minus qui génère l'echec

---
+++ src/test/java/com/example/demo/Exercice3.java
@@ -0,0 +1,12 @@
+// Exercice 3:
+
+/*On peut remerquer que toutes les methodes ne sont pas tester,
+ la methode mult est à 0% donc aucun test n'a été fait pour cette methode
+ donc il faudra ajouter un @Test et dedans implémenter pour mult()
+ tout les test on été réussi 
+
+Pour améliorer, il faut implémenter les tests necessaires donc pour la
+methode mult
+*/
+
+
---
+++ src/test/java/com/example/demo/TestCalculatrice.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCalculatrice {
+
+    Calculatrice calc = new Calculatrice();
+
+    @Test
+    public void TestAddition() {
+        assertEquals(5.0f, calc.add(2.0f, 3.0f));
+        assertEquals(-1.0f, calc.add(2.0f, -3.0f));
+    }
+
+    @Test
+    public void TestMultiplication() {
+        assertEquals(6.0f, calc.mult(2.0f, 3.0f));
+        assertEquals(0.0f, calc.mult(5.0f, 0.0f));
+    }
+
+    @Test
+    public void TestDivision() {
+        assertEquals(2.0f, calc.div(6.0f, 3.0f));
+        assertEquals(-2.5f, calc.div(5.0f, -2.0f));
+    }
+
+    @Test
+    public void testDivisionParZeroException() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            calc.div(10.0f, 0.0f);
+        });
+        assertEquals("Division par O", exception.getMessage());
+    }
+
+    @Test
+    public void testSoustraction() {
+        assertEquals(2.0f, calc.minus(5.0f, 3.0f));
+        assertEquals(-8.0f, calc.minus(-5.0f, 3.0f));
+        assertEquals(0.0f, calc.minus(4.5f, 4.5f));
+    }
+
+
+    @Test
+    public float minus(float operande1, float operande2) {
+
+        float Operande1 = operande1;
+        float Operande2 = operande2;
+
+        return assertEquals(5.0f, calc.minus(Operande1, Operande2));
+    }
+
+}
+
