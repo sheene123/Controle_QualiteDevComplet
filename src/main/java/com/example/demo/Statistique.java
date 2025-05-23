20a65c09eb40fbec41363686a26f1539a8ffec4f
SIMILIEN Carld <12300697@q10421.iutv.univ-paris13.fr>
2025-05-23 11:23:07 +0200

    exercice 1 fini avec minus qui génère l'echec

---
+++ src/main/java/com/example/demo/Statistique.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Statistique {
+    Calculatrice calculatrice = new Calculatrice();
+    public float moyenne(float x, float y){
+        return calculatrice.div(
+                calculatrice.add(x, y),
+                2);
+    }
+}
